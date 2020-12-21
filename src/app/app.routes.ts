44ce08fdbcd007e8eecbf904c1c19ff36ceb82ca
Mardoniosc <mardonio@live.com>
2020-12-21 15:35:47 -0300

    Inicializando a criaçao da pagina: 'Lista de filmes'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,20 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
+import { MovieListComponent } from './pages/movie-list/movie-list.component';
 
 
 export const routes: Routes = [
   {
       path: '',
-      redirectTo: '/about',
+      redirectTo: '/movies',
       pathMatch: 'full',
   },
   {
     path: 'about',
     component: AboutComponent
   },
+  {
+    path: 'movies',
+    component: MovieListComponent
+  }
 ];
