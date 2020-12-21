316fdec63d2b4a883e6e60a4af02a65d09d1656e
Mardoniosc <mardonio@live.com>
2020-12-21 14:26:09 -0300

    Configurando auto import

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -2,9 +2,6 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptModule } from '@nativescript/angular';
 import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
-import { HomeComponent } from '@src/app/home/home.component';
-
-
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from '@nativescript/angular';
@@ -15,7 +12,6 @@ import { HomeComponent } from '@src/app/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
   ],
   imports: [
     NativeScriptModule,
