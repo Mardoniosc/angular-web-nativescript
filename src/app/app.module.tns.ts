c9491b6c2b32f1d2a5fe313533773fbb7ca9cfab
Mardoniosc <mardonio@live.com>
2020-12-22 10:30:26 -0300

    Configurando a API de Filmes utilizando o 'in-memory-web-api'

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -1,28 +1,28 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { NativeScriptModule } from '@nativescript/angular';
-import { AppRoutingModule } from '@src/app/app-routing.module';
-import { AppComponent } from '@src/app/app.component';
-import { AboutComponent } from '@src/app/pages/about/about.component';
-import { MovieListComponent } from '@src/app/pages/movie-list/movie-list.component';
-
-// Uncomment and add to NgModule imports if you need to use two-way binding
-// import { NativeScriptFormsModule } from '@nativescript/angular';
-
-// Uncomment and add to NgModule imports  if you need to use the HTTP wrapper
-// import { NativeScriptHttpClientModule } from '@nativescript/angular';
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import {
+  NativeScriptHttpClientModule,
+  NativeScriptModule
+} from "@nativescript/angular";
+import { AppRoutingModule } from "@src/app/app-routing.module";
+import { AppComponent } from "@src/app/app.component";
+import { AboutComponent } from "@src/app/pages/about/about.component";
+import { MovieListComponent } from "@src/app/pages/movie-list/movie-list.component";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./in-memory-data.service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent,
-    MovieListComponent,
-  ],
+  declarations: [AppComponent, AboutComponent, MovieListComponent],
   imports: [
     NativeScriptModule,
+    NativeScriptHttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      put204: false,
+      dataEncapsulation: false,
+    }),
     AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: [NO_ERRORS_SCHEMA]
+  schemas: [NO_ERRORS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from '@src/app/app-routing.module';
-import { AppComponent } from '@src/app/app.component';
-import { AboutComponent } from '@src/app/pages/about/about.component';
-import { MovieListComponent } from '@src/app/pages/movie-list/movie-list.component';
-
-
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "@src/app/app-routing.module";
+import { AppComponent } from "@src/app/app.component";
+import { AboutComponent } from "@src/app/pages/about/about.component";
+import { MovieListComponent } from "@src/app/pages/movie-list/movie-list.component";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./in-memory-data.service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent,
-    MovieListComponent,
-  ],
+  declarations: [AppComponent, AboutComponent, MovieListComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      put204: false,
+      dataEncapsulation: false,
+    }),
     AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
