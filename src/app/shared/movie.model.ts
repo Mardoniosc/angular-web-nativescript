54c063e17c7b77d5067e18131a70f370289f465b
Mardoniosc <mardonio@live.com>
2020-12-22 10:55:04 -0300

    Adicionando o Model 'Movie'

---
+++ src/app/shared/movie.model.ts
@@ -0,0 +1,12 @@
+export class Movie {
+  constructor(
+    public id?: number,
+    public title?: string,
+    public obs?: string,
+    public toWatchNext?: boolean
+  ) {}
+
+  public static fromJson(jsonData: any): Movie {
+    return Object.assign(new Movie(), jsonData);
+  }
+}
